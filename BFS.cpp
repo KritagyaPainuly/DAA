4d78997d362e75a6083bb0a4525af462e0fe2f0f
Kritagya Painuly <75200248+KritagyaPainuly@users.noreply.github.com>
2022-06-19 14:08:54 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define max 100
+void BFS(int g[][max],int n,int start)
+{
+    int visted[n]={0};
+    list<int> queue;
+    visted[start]=1;
+    cout<<start<<" ";
+    queue.push_back(start);
+    while(!queue.empty())
+    {
+        start=queue.front();
+        queue.pop_front();
+        for(int i=1;i<=n;i++)
+        {
+            if(g[start][i]==1 && visted[i]!=1)
+            {
+                visted[i]=1;
+                queue.push_back(i);
+                cout<<i<<" ";
+            }
+        }
+    }
+}
+int main()
+{
+    int n,m;
+    cout<<"Number of vertices";
+    cin>>n;
+    int g[max][max]={0};
+    cout<<"Number of edges";
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        g[x][y]=1;
+        g[y][x]=1;
+    }
+    for(int i=1;i<n+1;i++){
+    for(int j=1;j<n+1;j++)
+    cout<<g[i][j]<<" ";
+    cout<<endl;}
+    BFS(g,n,4);
+}
